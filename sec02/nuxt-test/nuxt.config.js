6adac08a71d2d87c8a35342582067b92b557530e
taka01150810 <taka01150810@gmail.com>
2023-06-25 13:28:05 +0900

    NextJSを使ってみる(フォルダ名なしでオートインポートする方法)

--- sec02/nuxt-test/nuxt.config.js
+++ sec02/nuxt-test/nuxt.config.js
@@ -24,7 +24,12 @@ export default {
   plugins: [],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
-  components: true,
+  components: {
+    dirs: [
+      '~/components/',
+      '~/components/base'
+    ]
+   },
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
