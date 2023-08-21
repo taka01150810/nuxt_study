9f518df5649a53174d3b2a92a5317219ac9f4525
taka01150810 <taka01150810@gmail.com>
2023-08-21 13:17:52 +0900

    NuxtJSを使ってみる(plugin(dayjsを使ってみる))

--- sec02/nuxt-test/nuxt.config.js
+++ sec02/nuxt-test/nuxt.config.js
@@ -27,7 +27,10 @@ export default {
   css: [],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [],
+  // Vueインスタンス化する前に呼び出される
+  plugins: [
+    '@/plugins/dayjs',
+  ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
--- sec02/nuxt-test/pages/about.vue
+++ sec02/nuxt-test/pages/about.vue
@@ -1,12 +1,19 @@
 <template>
   <div>
     aboutです
+    <template>{{ now }}</template>
   </div>
 </template>
 
 <script>
 export default {
-
+  data(){
+    return {
+      now: null
+  }},
+  mounted(){
+    this.now = this.$dayjs().format('YYYY-MM-DD:HH:mm:ss')
+  }
 }
 </script>
 
