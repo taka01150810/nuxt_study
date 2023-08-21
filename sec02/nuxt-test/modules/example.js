dfe9d9f0749357628d8bc14237f4e1dc4a3bd846
taka01150810 <taka01150810@gmail.com>
2023-08-21 13:32:07 +0900

    NuxtJSを使ってみる(modules その2)

--- sec02/nuxt-test/modules/example.js
+++ sec02/nuxt-test/modules/example.js
@@ -3,6 +3,7 @@ export default function (){
     // Hookでどのタイミングで実施するか指定もできる
     // このthisはModuleContainerと呼ばれるオブジェクト
     this.nuxt.hook('ready', async nuxt => {
+        // console.log(this)
         console.log('Nuxt is ready')
     })
 }
