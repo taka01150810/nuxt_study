8ea0469aa920048fdf40c4f83c458ef21ce628e6
taka01150810 <taka01150810@gmail.com>
2023-08-21 12:00:26 +0900

    NuxtJSを使ってみる(asyncData,fetch(引数ありなし))

--- sec02/nuxt-test/pages/index.vue
+++ sec02/nuxt-test/pages/index.vue
@@ -9,6 +9,27 @@
 
 <script>
 export default {
-  name: 'IndexPage',
+
+  asyncData(){
+    console.log('asyncDataです')
+  },
+
+  created(){
+    console.log('createdです')
+  },
+
+  // 引数なしと引数ありでライフサイクルが異なる
+  // 引数なし
+  // fetch(){
+  //   console.log('fetchです')
+  // },
+
+  // 引数あり
+  fetch( context ){
+    console.log('fetchです')
+    console.log(context.route)
+    console.log(context.route.name)
+  }
+
 }
 </script>
