8a8050a7fa94007d1e77c1b1f663812d23f80e67
taka01150810 <taka01150810@gmail.com>
2023-08-21 19:05:32 +0900

    NuxtでGoogleBooksAPI(Header, Footerの切り離し)

---
+++ sec03/bookapp/components/Header.vue
@@ -0,0 +1,62 @@
+<template>
+  <div>
+    <v-navigation-drawer
+      v-model="drawer"
+      fixed
+      app
+    >
+      <v-list>
+        <v-list-item
+          v-for="(item, i) in items"
+          :key="i"
+          :to="item.to"
+          router
+          exact
+        >
+          <v-list-item-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    <v-app-bar
+      :clipped-left="clipped"
+      fixed
+      app
+    >
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+      <v-toolbar-title>{{ title }}</v-toolbar-title>
+    </v-app-bar>
+  </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            clipped: false,
+            drawer: false,
+            items: [
+                {
+                    icon: 'mdi-apps',
+                    title: 'Welcome',
+                    to: '/'
+                },
+                {
+                    icon: 'mdi-chart-bubble',
+                    title: 'Inspire',
+                    to: '/inspire'
+                }
+            ],
+            title: 'BookApp'
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
