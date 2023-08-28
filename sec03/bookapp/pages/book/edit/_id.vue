5af7c5a21a244cf9fccf22c36971a16dee8b7d3e
taka01150810 <taka01150810@gmail.com>
2023-08-28 17:35:26 +0900

    本管理(beforeRouteEnter(ナビゲーションガード))

--- sec03/bookapp/pages/book/edit/_id.vue
+++ sec03/bookapp/pages/book/edit/_id.vue
@@ -2,17 +2,29 @@
   <div> 
     {{ books[$route.params.id] }}
     {{ books[$route.params.id].title }}
+    {{ book.title }}
   </div>
 </template>
 
 <script>
 export default {
+  //ページ遷移時に実行したいのでvue-routerのナビゲーションガードを使う
+  beforeRouteEnter(to, from, next){
+    next( vm => {
+      vm.book = vm.books[vm.$route.params.id]
+    })
+  },
   props: {
     books: {
       type: Array,
       default: ()=>{}
     }
   },
+  data(){
+    return {
+      book: '',
+    }
+  }
 }
 </script>
 
